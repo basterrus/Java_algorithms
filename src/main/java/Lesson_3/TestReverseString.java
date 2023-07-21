00e1a5ec1e69eda75c42c388d9e83d53fd58a66d
baster <pnedoshivin@gmail.com>
2023-07-21 19:08:55 +0300

    Lesson_3

---
+++ src/main/java/Lesson_3/TestReverseString.java
@@ -0,0 +1,7 @@
+package Lesson_3;
+
+public class TestReverseString {
+    public static void main(String[] args) {
+        System.out.println(StringReverse.stringReverse("Hello world!"));
+    }
+}
---
+++ src/main/java/Lesson_3/queue/Queue.java
@@ -0,0 +1,19 @@
+package Lesson_3.queue;
+
+public interface Queue<E> {
+
+    boolean insert(E value);
+
+    E remove();
+
+    E peekFront();
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
